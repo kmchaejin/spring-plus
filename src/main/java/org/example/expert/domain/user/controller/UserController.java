643aea4fc33f951a9df0e136f9c60591def50b5b
kmchaejin <chaejin040@gmail.com>
2025-05-14 15:50:22 +0900

    feat : level 3-12 aws 활용

--- src/main/java/org/example/expert/domain/user/controller/UserController.java
+++ src/main/java/org/example/expert/domain/user/controller/UserController.java
@@ -1,6 +1,11 @@
 package org.example.expert.domain.user.controller;
 
+import java.io.IOException;
+
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
+
 import org.example.expert.domain.common.dto.AuthUser;
 import org.example.expert.domain.user.dto.request.UserChangePasswordRequest;
 import org.example.expert.domain.user.dto.response.UserResponse;
@@ -9,6 +14,9 @@ import org.example.expert.security.CustomUserPrincipal;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +33,16 @@ public class UserController {
     public void changePassword(@AuthenticationPrincipal CustomUserPrincipal userPrincipal, @RequestBody UserChangePasswordRequest userChangePasswordRequest) {
         Long userId = Long.parseLong(userPrincipal.getUsername());
         userService.changePassword(userId, userChangePasswordRequest);
+    }
+
+    @PostMapping("/users/profileImage")
+    public void imageUpload(@AuthenticationPrincipal CustomUserPrincipal userPrincipal, HttpServletRequest request) throws IOException {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(request.getContentType());
+        metadata.setContentLength(request.getContentLengthLong());
+
+        AmazonS3Client amazonS3Client = new AmazonS3Client();
+        String key = "myfolder/" + userPrincipal.getUsername();
+        amazonS3Client.putObject("chaejinkim-bucket", key, request.getInputStream(), metadata);
     }
 }
